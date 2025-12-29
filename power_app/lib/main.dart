290c4ded3a2ee022dff54a57a79414c200a88e4e
zxh <545676500@qq.com>
2025-12-29 17:47:03 +0800

    slidebar integration

--- power_app/lib/main.dart
+++ power_app/lib/main.dart
@@ -17,7 +17,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeFrame extends StatelessWidget {
+class HomeFrame extends StatefulWidget {
+  @override
+  State<HomeFrame> createState() => _HomeFrameState();
+}
+
+class _HomeFrameState extends State<HomeFrame> {
+  double width = 400;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -27,13 +34,18 @@ class HomeFrame extends StatelessWidget {
         Expanded(
           child: ResizablePanel.horizontal(
             children: [
-              const ResizablePane(
-                initialSize: 400,
-                child: SlideNav(title: 'title'),
+              ResizablePane(
+                initialSize: width,
+                child: SlideNav(title: 'title', width: width),
+                onSizeChangeEnd: (w) {
+                  setState(() {
+                    width = w;
+                  });
+                },
               ),
               ResizablePane(
-                initialSize: MediaQuery.of(context).size.width - 400,
-                child: Expanded(child: ContentZone()),
+                initialSize: MediaQuery.of(context).size.width - width,
+                child: ContentZone(),
               ),
             ],
           ),
@@ -51,9 +63,10 @@ class ContentZone extends StatelessWidget {
 }
 
 class SlideNav extends StatefulWidget {
-  const SlideNav({super.key, required this.title});
+  const SlideNav({super.key, required this.title, required double this.width});
 
   final String title;
+  final double width;
 
   @override
   State<SlideNav> createState() => _SlideNavState();
@@ -68,48 +81,90 @@ class _SlideNavState extends State<SlideNav> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      child: OutlinedContainer(
-        child: NavigationSidebar(
-          index: selected,
-          onSelected: (index) {
-            setState(() {
-              selected = index;
-            });
-          },
-          children: [
-            const NavigationLabel(child: Text('Discovery')),
-            buildButton('Listen Now', BootstrapIcons.playCircle),
-            buildButton('Browse', BootstrapIcons.grid),
-            buildButton('Radio', BootstrapIcons.broadcast),
-            const NavigationGap(24),
-            const NavigationDivider(),
-            const NavigationLabel(child: Text('Library')),
-            buildButton('Playlist', BootstrapIcons.musicNoteList),
-            buildButton('Songs', BootstrapIcons.musicNote),
-            buildButton('For You', BootstrapIcons.person),
-            buildButton('Artists', BootstrapIcons.mic),
-            buildButton('Albums', BootstrapIcons.record2),
-            const NavigationGap(24),
-            const NavigationDivider(),
-            const NavigationLabel(child: Text('Playlists')),
-            buildButton('Recently Added', BootstrapIcons.musicNoteList),
-            buildButton('Recently Played', BootstrapIcons.musicNoteList),
-            buildButton('Top Songs', BootstrapIcons.musicNoteList),
-            buildButton('Top Albums', BootstrapIcons.musicNoteList),
-            buildButton('Top Artists', BootstrapIcons.musicNoteList),
-            buildButton(
-              'Logic Discography With Some Spice',
-              BootstrapIcons.musicNoteList,
-            ),
-            buildButton('Bedtime Beats', BootstrapIcons.musicNoteList),
-            buildButton('Feeling Happy', BootstrapIcons.musicNoteList),
-            buildButton('I miss Y2K Pop', BootstrapIcons.musicNoteList),
-            buildButton('Runtober', BootstrapIcons.musicNoteList),
-          ],
-        ),
-      ),
+    print(widget.width);
+    return Builder(
+      builder: (context) {
+        print(MediaQuery.of(context).size);
+        return SizedBox(
+          width: widget.width,
+          child: OutlinedContainer(
+            child: widget.width > 100
+                ? NavigationSidebar(
+                    index: selected,
+                    onSelected: (index) {
+                      setState(() {
+                        selected = index;
+                      });
+                    },
+                    children: [
+                      const NavigationLabel(child: Text('Discovery')),
+                      buildButton('Listen Now', BootstrapIcons.playCircle),
+                      buildButton('Browse', BootstrapIcons.grid),
+                      buildButton('Radio', BootstrapIcons.broadcast),
+                      const NavigationGap(24),
+                      const NavigationDivider(),
+                      const NavigationLabel(child: Text('Library')),
+                      buildButton('Playlist', BootstrapIcons.musicNoteList),
+                      buildButton('Songs', BootstrapIcons.musicNote),
+                      buildButton('For You', BootstrapIcons.person),
+                      buildButton('Artists', BootstrapIcons.mic),
+                      buildButton('Albums', BootstrapIcons.record2),
+                      const NavigationGap(24),
+                      const NavigationDivider(),
+                      const NavigationLabel(child: Text('Playlists')),
+                      buildButton(
+                        'Recently Added',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton(
+                        'Recently Played',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton('Top Songs', BootstrapIcons.musicNoteList),
+                      buildButton('Top Albums', BootstrapIcons.musicNoteList),
+                      buildButton('Top Artists', BootstrapIcons.musicNoteList),
+                      buildButton(
+                        'Logic Discography With Some Spice',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton(
+                        'Bedtime Beats',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton(
+                        'Feeling Happy',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton(
+                        'I miss Y2K Pop',
+                        BootstrapIcons.musicNoteList,
+                      ),
+                      buildButton('Runtober', BootstrapIcons.musicNoteList),
+                    ],
+                  )
+                : NavigationRail(
+                    index: selected,
+                    onSelected: (index) {
+                      setState(() {
+                        selected = index;
+                      });
+                    },
+                    children: [
+                      buildButton('Home', BootstrapIcons.house),
+                      buildButton('Explore', BootstrapIcons.compass),
+                      buildButton('Library', BootstrapIcons.musicNoteList),
+                      const NavigationDivider(),
+                      const NavigationLabel(child: Text('Settings')),
+                      buildButton('Profile', BootstrapIcons.person),
+                      buildButton('App', BootstrapIcons.appIndicator),
+                      const NavigationDivider(),
+                      const NavigationGap(12),
+                      const NavigationWidget(child: FlutterLogo()),
+                    ],
+                  ),
+          ),
+        );
+      },
     );
   }
 }
